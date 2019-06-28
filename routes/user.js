42f88f7d6cc2dcb2f6db868a13701ce3de879e57
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-06-28 15:40:39 +0800

    Add user registration feature and fix bug in register page

--- routes/user.js
+++ routes/user.js
@@ -20,7 +20,30 @@ router.get('/register', (req, res) => {
 
 // register check
 router.post('/register', (req, res) => {
-    res.send('register')
+    console.log('req.body', req.body)
+    const { name, email, password, password2 } = req.body
+    User.findOne({ email: email }).then(user => {
+        if (user) {
+            // 如果 email 已經存在的話，將不能送出，並回到註冊表單頁面
+            console.log('User already exists')
+            res.render('register', { name, email, password, password2 })
+        } else {
+            // 如果 email 不存在就新增使用者
+            // 新增完成後導回首頁
+            console.log('Add new user: ', user)
+            const newUser = new User({
+                name,
+                email,
+                password
+            })
+            newUser
+                .save()
+                .then(user => {
+                    res.redirect('/')
+                })
+                .catch(err => console.log(err))
+        }
+    })
 })
 
 // logout
