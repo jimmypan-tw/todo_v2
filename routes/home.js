5ba70501e417d007c69803a3d6b0307ca4bece87
Jimmy Pan <jimmypan@ntu.edu.tw>
2019-06-29 10:17:42 +0800

    Add user authentication in todo and home routers

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../models/todo')
 
+// 載入 auth middleware 裡的authenticated 方法
+const { authenticated } = require('../config/auth')
+
+
 // 設定首頁路由器
 // Todo 首頁
-router.get('/', (req, res) => {
+// 加入 authenticated 驗證
+router.get('/', authenticated, (req, res) => {
     Todo.find({})
         .sort({ name: 'asc' })
         .exec((err, todos) => {
--- routes/todo.js
+++ routes/todo.js
@@ -2,20 +2,22 @@ const express = require('express')
 const router = express.Router()
 const Todo = require('../models/todo')
 
+// 載入auth middleware
+const { authenticated } = require('../config/auth')
 
 // 列出全部 Todo
-router.get('/', (req, res) => {
+router.get('/', authenticated, (req, res) => {
     res.send('列出所有 Todo')
 })
 
 // 新增一筆 Todo 頁面
-router.get('/new', (req, res) => {
+router.get('/new', authenticated, (req, res) => {
     console.log('I am GET /new')
     return res.render('new')
 })
 
 // 顯示一筆 Todo 的詳細內容
-router.get('/:id', (req, res) => {
+router.get('/:id', authenticated, (req, res) => {
     Todo.findById(req.params.id, (err, todo) => {
         if (err) return console.error(err)
         return res.render('detail', { todo: todo })
@@ -23,7 +25,7 @@ router.get('/:id', (req, res) => {
 })
 
 // 新增一筆  Todo
-router.post('/', (req, res) => {
+router.post('/', authenticated, (req, res) => {
     const todo = new Todo({
         name: req.body.name
     })
@@ -35,7 +37,7 @@ router.post('/', (req, res) => {
 })
 
 // 修改 Todo 頁面
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', authenticated, (req, res) => {
     Todo.findById(req.params.id, (err, todo) => {
         if (err) return console.error(err)
         return res.render('edit', { todo: todo })
@@ -43,7 +45,7 @@ router.get('/:id/edit', (req, res) => {
 })
 
 // 修改 Todo
-router.put('/:id', (req, res) => {
+router.put('/:id', authenticated, (req, res) => {
     Todo.findById(req.params.id, (err, todo) => {
         if (err) return console.error(err)
         todo.name = req.body.name
@@ -60,7 +62,7 @@ router.put('/:id', (req, res) => {
 })
 
 // 刪除 Todo
-router.delete('/:id/delete', (req, res) => {
+router.delete('/:id/delete', authenticated, (req, res) => {
     Todo.findById(req.params.id, (err, todo) => {
         if (err) return console.error(err)
         todo.remove(err => {
